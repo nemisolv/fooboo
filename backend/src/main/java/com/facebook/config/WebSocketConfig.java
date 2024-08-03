9c7495a8529dc5667d6757d4f7d562ac0d63ddda
nemisolv <nemisolv@gmail.com>
2024-08-03 22:57:28 +0700

    fix websocket v2

--- backend/src/main/java/com/facebook/config/WebSocketConfig.java
+++ backend/src/main/java/com/facebook/config/WebSocketConfig.java
@@ -30,7 +30,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/ws")
+        registry.addEndpoint("/ws","/wss")
                 .setAllowedOrigins(Constants.CLIENT_BASE_URL)
 
                 .withSockJS()
